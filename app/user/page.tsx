a7e535eb1ce79599b7ac33ea6de1b78b8d4fae80
Himanshu Kiran <himanshukiran799@gmail.com>
2024-04-10 23:53:03 +0530

    added

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 async function fetchData() {
     const response = await axios.get("http://localhost:3000/api/user")
-    
     await new Promise(r=>setTimeout(r,3000))
     console.log("Response is " + JSON.stringify(response.data))
     return response.data;
